3014583148f29dae14e24e4730249f6229740141
peco2282 <pecop2282@gmail.com>
2025-07-14 17:11:36 +0900

    initial commit

---
+++ app/src/main/java/com/github/peco2282/MainActivity.kt
@@ -0,0 +1,204 @@
+package com.github.peco2282
+
+import android.Manifest
+import android.content.ComponentName
+import android.content.Intent
+import android.content.ServiceConnection
+import android.content.pm.PackageManager
+import android.net.wifi.WifiManager
+import android.os.Build
+import android.os.Bundle
+import android.os.IBinder
+import android.widget.Toast
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
+import com.github.peco2282.ui.theme.PowerAcquisitionTheme
+import kotlinx.coroutines.flow.StateFlow
+
+
+class MainActivity : ComponentActivity() {
+    private var wifiManager: WifiManager? = null
+    private var serviceIntent: Intent? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager
+        enableEdgeToEdge()
+//        setContent {
+//            PowerAcquisitionTheme {
+//                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+//                    Greeting(
+//                        name = "Android",
+//                        modifier = Modifier.padding(innerPadding)
+//                    )
+//                }
+//            }
+//        }
+        // サービスにバインドする前に、初期のUIを表示
+        setContent {
+            AppContent(
+                null, // サービスがまだ接続されていないのでnull
+                ::startRssiMonitoringService,
+                ::stopRssiMonitoringService
+            )
+        }
+
+        checkLocationPermission()
+    }
+
+    private fun checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+            != PackageManager.PERMISSION_GRANTED
+        ) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                PERMISSION_REQUEST_CODE
+            )
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String?>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_REQUEST_CODE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                println("Permission granted")
+            } else {
+                println("Permission denied!")
+            }
+        }
+    }
+
+    val PERMISSION_REQUEST_CODE: Int = 1001
+
+    private var wifiRssiMonitoringService: WifiRssiMonitoringService? = null
+    private var isBound = false
+
+    private val connection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            val binder = service as WifiRssiMonitoringService.WifiRssiServiceBinder
+            wifiRssiMonitoringService = binder.service
+            isBound = true
+            // サービス接続後、UIの更新を開始
+            setContent {
+                AppContent(
+                    wifiRssiMonitoringService?.currentRssi as StateFlow<Int>,
+                    ::startRssiMonitoringService,
+                    ::stopRssiMonitoringService
+                )
+            }
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            wifiRssiMonitoringService = null
+            isBound = false
+        }
+    }
+
+    private fun startRssiMonitoringService() {
+        serviceIntent = Intent(this, WifiRssiMonitoringService::class.java)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            ContextCompat.startForegroundService(this, serviceIntent!!)
+        } else {
+            startService(serviceIntent!!)
+        }
+        bindService(serviceIntent!!, connection, BIND_AUTO_CREATE)
+        Toast.makeText(this, "RSSI監視を開始しました", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun stopRssiMonitoringService() {
+        if (isBound) {
+            unbindService(connection)
+            isBound = false
+            wifiRssiMonitoringService = null // Clear the service reference
+        }
+
+        if (serviceIntent != null) {
+            stopService(serviceIntent)
+            Toast.makeText(this, "RSSI監視を停止しました", Toast.LENGTH_SHORT).show()
+        } else {
+            Toast.makeText(this, "サービスが開始していません", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
+
+@Composable
+fun AppContent(
+    rssiStateFlow: StateFlow<Int>?,
+    onStartServiceClick: () -> Unit,
+    onStopServiceClick: () -> Unit
+) {
+    PowerAcquisitionTheme {
+        Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(innerPadding),
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                // RSSIを表示するComposable
+                RssiDisplay(rssiStateFlow)
+
+                Button(
+                    onClick = onStartServiceClick,
+                    modifier = Modifier.padding(top = 16.dp)
+                ) {
+                    Text("RSSI監視を開始")
+                }
+                Button(
+                    onClick = onStopServiceClick,
+                    modifier = Modifier.padding(top = 8.dp)
+                ) {
+                    Text("RSSI監視を停止")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun RssiDisplay(rssiStateFlow: StateFlow<Int>?) {
+    // StateFlowをComposeのStateとして収集
+    // `collectAsStateWithLifecycle` を使用すると、ライフサイクルに安全に収集できる
+    val rssi by rssiStateFlow?.collectAsStateWithLifecycle() ?: remember { mutableIntStateOf(-1000) }
+
+    val displayRssiText = when (rssi) {
+        -1000 -> {
+            "RSSI: 測定待機中 / サービス未開始"
+        }
+
+        Integer.MIN_VALUE -> { // サービス内の初期値またはエラー値
+            "RSSI: Wi-Fi未接続"
+        }
+
+        else -> {
+            "現在のRSSI: $rssi dBm"
+        }
+    }
+
+    Text(
+        text = displayRssiText,
+        modifier = Modifier.padding(16.dp)
+    )
+}
---
+++ app/src/main/java/com/github/peco2282/WifiRssiMonitoringService.kt
@@ -0,0 +1,187 @@
+package com.github.peco2282
+
+//noinspection SuspiciousImport
+import android.R
+import android.app.*
+import android.content.Intent
+import android.net.wifi.WifiManager
+import android.os.Binder
+import android.os.Build
+import android.os.Handler
+import android.os.IBinder
+import android.util.Log
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.core.app.NotificationCompat
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import java.io.File
+import java.io.FileOutputStream
+import java.io.OutputStreamWriter
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+
+
+class WifiRssiMonitoringService : Service() {
+    @RequiresApi(Build.VERSION_CODES.O)
+    companion object {
+        const val CHANNEL_ID: String = "WifiRssiMonitoringChannel"
+
+        const val NOTIFICATION_ID: Int = 123
+
+        const val UPDATE_INTERVAL: Long = 2000
+
+        val FNAME_FORMATTER: DateFormat = SimpleDateFormat("'wifi_'yyyyMMdd_HHmmss.'csv'", Locale.JAPAN)
+
+        val DATE_FORMATTER: DateFormat = SimpleDateFormat("yyyy-MM-dd_HH:mm:ss.SSS", Locale.JAPAN)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private var currentFName: String? = FNAME_FORMATTER.format(Date())
+
+    private var handler: Handler? = null
+    private var updateRssiRunnable: Runnable? = null
+    private var isStarted = false
+
+    // RSSI値を公開するためのStateFlow
+    private val _currentRssi = MutableStateFlow(-1000) // 初期値は無効なRSSI
+    val currentRssi: StateFlow<Int>? = _currentRssi
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onCreate() {
+        super.onCreate()
+        createNotificationChannel()
+
+        handler = Handler()
+
+        updateRssiRunnable = object : Runnable {
+            override fun run() {
+                if (!isStarted) return
+                updateRssi()
+                handler!!.postDelayed(this, UPDATE_INTERVAL)
+            }
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        val notification = buildNotification("RSSI: 測定中...")
+        startForeground(NOTIFICATION_ID, notification)
+
+        handler!!.post(updateRssiRunnable!!)
+        val data = "Date,RSSI"
+
+        val txt = "保存先: ${saveToExternalFilesDir(data)?.absolutePath}"
+        Toast.makeText(this, txt, Toast.LENGTH_LONG).show()
+        isStarted = true
+        Log.i("WifiRssiService", "Service started")
+        return START_STICKY
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun saveToExternalFilesDir(data: String): File? {
+        val appSpecificDir = getExternalFilesDir(null)
+        if (appSpecificDir == null) {
+            println("Service: 外部ストレージのアプリ専用ディレクトリが見つかりません。")
+            return null
+        }
+
+        val file = File(appSpecificDir, currentFName!!)
+        try {
+            FileOutputStream(file, true).use { outputStream ->
+                OutputStreamWriter(outputStream, Charsets.UTF_8).use { writer ->
+                    writer.write(data)
+                    writer.appendLine()
+                    writer.flush()
+                }
+            }
+            return file
+        } catch (e: Exception) {
+            e.printStackTrace()
+            println("Service: データ保存に失敗しました。エラー: ${e.message}")
+            return null
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onDestroy() {
+        super.onDestroy()
+        handler!!.removeCallbacks(updateRssiRunnable!!)
+        Log.i("WifiRssiService", "Service destroyed")
+        _currentRssi.value = -1000
+        currentFName = null
+        isStarted = false
+    }
+
+    override fun onBind(intent: Intent?): IBinder {
+        return WifiRssiServiceBinder()
+    }
+
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    private fun updateRssi() {
+        val wifiManager = applicationContext.getSystemService(WIFI_SERVICE) as WifiManager?
+        if (wifiManager != null) {
+            val wifiInfo = wifiManager.connectionInfo
+            if (wifiInfo != null) {
+                val rssi = wifiInfo.rssi
+                _currentRssi.value = rssi // StateFlowを更新
+                val contentText = "RSSI: $rssi dBm"
+                saveToExternalFilesDir(DATE_FORMATTER.format(Date()) + "," + rssi.toString())
+                Log.d("WifiRssiService", contentText)
+
+                val notification = buildNotification(contentText)
+                val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager?
+                notificationManager?.notify(NOTIFICATION_ID, notification)
+            } else {
+                Log.w("WifiRssiService", "WifiInfo is null")
+                _currentRssi.value = -1000 // Wi-Fi未接続などで無効な値
+                val notification = buildNotification("Wi-Fi未接続")
+                val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager?
+                notificationManager?.notify(NOTIFICATION_ID, notification)
+            }
+        } else {
+            Log.e("WifiRssiService", "WifiManager is null")
+            _currentRssi.value = -1000
+        }
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name: CharSequence = "Wi-Fi RSSI Monitor"
+            val description = "Wi-FiのRSSIを監視します"
+            val importance = NotificationManager.IMPORTANCE_LOW
+            val channel = NotificationChannel(CHANNEL_ID, name, importance)
+            channel.description = description
+
+            val notificationManager = getSystemService<NotificationManager?>(NotificationManager::class.java)
+            notificationManager?.createNotificationChannel(channel)
+        }
+    }
+
+    private fun buildNotification(contentText: String): Notification {
+        val notificationIntent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(
+            this,
+            0,
+            notificationIntent,
+            PendingIntent.FLAG_IMMUTABLE
+        )
+
+        return NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Wi-Fi RSSI Monitor")
+            .setContentText(contentText)
+            .setSmallIcon(R.drawable.ic_dialog_info)
+            .setContentIntent(pendingIntent)
+            .setPriority(NotificationCompat.PRIORITY_LOW)
+            .setOngoing(true)
+            .build()
+    }
+
+   inner class WifiRssiServiceBinder : Binder() {
+        val service: WifiRssiMonitoringService
+            get() = this@WifiRssiMonitoringService
+    }
+}
