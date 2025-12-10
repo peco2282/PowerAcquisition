ad4f604930b20dbd4b2fbd672a4fec3f89d213d5
peco2282 <koukahachi0622@outlook.jp>
2025-12-10 10:20:35 +0900

    build: enable Javadoc and sources download in IDEA configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,13 @@ plugins {
     id("com.android.application") version "8.10.0" apply false
     id("org.jetbrains.kotlin.android") version "2.0.21" apply false
     id("org.jetbrains.kotlin.plugin.compose") version "2.0.21" apply false
+    idea
+}
+
+
+idea {
+    module {
+        isDownloadJavadoc = true
+        isDownloadSources = true
+    }
 }
